e53b6cbe642ea6920607dff0ba418a61f5c504fb
leonardopadillla <84750145+leonardopadillla@users.noreply.github.com>
2023-04-22 07:30:22 -0600

    No existe ningun JoelOrtiz11
    Se trabajo el repositorio desde el centro de computo y el repositorio agrego a un tal joel como colaborador, tomo la cuenta iniciada sesion de ahi

--- formulariosrazorpractica/Models/estados_equipos.cs
+++ formulariosrazorpractica/Models/estados_equipos.cs
@@ -10,5 +10,7 @@ namespace  formulariosrazorpractica.Models
         public int id_estados_equipo {get; set;}
         public string descripcion {get; set;}
         public string estado {get; set;}
+
+        //Yep
     }
 }
